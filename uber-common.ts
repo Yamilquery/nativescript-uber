ddb57676955c187453d032ceeeb21b0eb6db3134
Yamil Díaz Aguirre <yamildiazaguirre@MacBook-Pro-de-Yamil.local>
2017-05-01 14:00:26 -0500

    debug: 2

---
+++ uber-common.ts
@@ -0,0 +1,5 @@
+import { View } from "ui/core/view";
+
+export abstract class UberView extends View /* implements IMapView */ {
+	
+}
---
+++ uber.ios.ts
@@ -0,0 +1,16 @@
+import { UberView as UberViewCommon } from "./uber-common";
+
+export class UberView extends UberViewCommon {
+	constructor() {
+        super();
+    }
+
+    applyUberToView() {
+    	/*
+    	let location = CLLocation(latitude: 37.787654, longitude: -122.402760)
+		let parameters = RideParametersBuilder().setPickupLocation(location).build()
+		let rideRequestView = RideRequestView(rideParameters: parameters, frame: view.bounds)
+		view.addSubview(rideRequestView);
+		*/
+    }
+}
